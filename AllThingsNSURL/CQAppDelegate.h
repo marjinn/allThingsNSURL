a4793e1947d9b0ada73ab3d5b3454002ee8a5e62
marjinn <marjinn@live.com>
2013-12-11 01:41:33 +0530

    Initial Commit

---
+++ AllThingsNSURL/CQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CQAppDelegate.h
+//  AllThingsNSURL
+//
+//  Created by mar Jinn on 12/11/13.
+//  Copyright (c) 2013 mar Jinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AllThingsNSURL/CQViewController.m
@@ -0,0 +1,29 @@
+//
+//  CQViewController.m
+//  AllThingsNSURL
+//
+//  Created by mar Jinn on 12/11/13.
+//  Copyright (c) 2013 mar Jinn. All rights reserved.
+//
+
+#import "CQViewController.h"
+
+@interface CQViewController ()
+
+@end
+
+@implementation CQViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
